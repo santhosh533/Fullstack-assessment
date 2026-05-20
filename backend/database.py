56d40bb1784d10b50349ed8621b6d0e8c9fc12c3
santhosh533 <santhoshmani419@gmail.com>
2026-05-20 10:41:24 +0530

    Fix database URL from env

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "mysql+pymysql://root:%40Santhosh420@127.0.0.1:3306/assessment_db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
